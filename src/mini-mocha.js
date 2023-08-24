07071925fb4f667e017bd02182af01619af018e6
mykas <mykolass@wix.com>
2023-08-24 14:12:07 +0300

    1 file modified

--- src/mini-mocha.js
+++ src/mini-mocha.js
@@ -1,11 +1,46 @@
 let count = 0;
+let testNumber = 0;
+const errorList = [];
 
-global.it = function(description, fn) {
-  fn();
-  console.log(`  ✓ ${description}`);
-  count++;
+const codeToError = {
+  ERR_ASSERTION: 'AssertionError',
+};
+
+const log = ({ message, startIdent = 4, CRLF = true }) => {
+  const ident = Array(startIdent).reduce((accu, current) => accu + 'd', 'l');
+  console.log(`${ident} ${message}${CRLF ? '\n' : ''}`);
+};
+
+const throwFailedTest = ({ error, description }) => {
+  const errorMessage = codeToError[error.code];
+  const { code, actual, operator, expected } = error;
+  console.log(`${description}:`);
+  console.log('');
+  console.log(
+    `      ${errorMessage} [${code}]: ${actual} ${operator} ${expected} `
+  );
+};
+
+global.it = function (description, fn) {
+  testNumber++;
+  try {
+    fn();
+    count++;
+  } catch (e) {
+    errorList.push({
+      description: `  ${testNumber}) ${description}`,
+      error: e,
+    });
+  }
+  log({ message: `${testNumber}) ${description}` });
 };
 
 require(process.argv[2]);
-console.log('');
-console.log(`  ${count} passing`);
+
+log({ message: `${count} passing`, CRLF: false });
+
+if (errorList.length > 0) {
+  console.log(`  ${errorList.length} failing`);
+  console.log('');
+  errorList.forEach(throwFailedTest);
+}
